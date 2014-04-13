9e998694f4c818277af0a7a8c85f17005539c1c2
alexthered <hiep.dv179@gmail.com>
2014-04-13 10:44:10 +0200

    Update triangle grid and report file

--- group19/heightmap_fshader.glsl
+++ group19/heightmap_fshader.glsl
@@ -1,5 +1,10 @@
 #version 330 core
 
-void main() {
+in vec2 UV;
 
+out vec3 color;
+uniform sampler2D heightMap;
+
+void main(){
+	color = vec3(1.0,1.0,0.0);
 }
--- group19/heightmap_vshader.glsl
+++ group19/heightmap_vshader.glsl
@@ -1,5 +1,14 @@
 #version 330 core
 
+
+in vec3 position;
+
+ // Output data ; will be interpolated for each fragment.
+out vec2 UV;
+
 void main() {
+	gl_Position =  vec4(position,1.0);
+	UV = (position.xy+vec2(1,1))/2.0;
 
 }
+
