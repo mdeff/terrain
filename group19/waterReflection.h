38d0ddd6ad278928f519f9eb2acf5d5f928deb9b
Michaël Defferrard <michael.defferrard@epfl.ch>
2014-05-25 13:05:08 +0200

    Quick fix

--- group19/waterReflection.h
+++ group19/waterReflection.h
@@ -27,7 +27,7 @@ private:
 	unsigned int _lightMVPID;
 
 	//helper function
-	GLuint WaterReflection::load_texture(const char * imagepath) const; 
+    GLuint load_texture(const char * imagepath) const;
 
 };
 
