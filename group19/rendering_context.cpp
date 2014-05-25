66ee01bc55b4714305d5dd075ede63156a25248c
Michaël Defferrard <michael.defferrard@epfl.ch>
2014-05-25 23:39:24 +0200

    No more need to have the program in use to retrieve uniform IDs thanks to glProgramUniform

--- group19/rendering_context.cpp
+++ group19/rendering_context.cpp
@@ -29,7 +29,6 @@ void RenderingContext::init(Vertices* vertices, const char* vshader, const char*
     _programID = opengp::compile_shaders(vshader, fshader, gshader, NULL, NULL);
     if(!_programID)
         exit(EXIT_FAILURE);
-    glUseProgram(_programID);
 
     /// Bind the vertex attribute ID to vertex data, if they exist.
     if(vertices != NULL && vertexAttribName != NULL) {
@@ -93,7 +92,7 @@ void RenderingContext::set_texture(const GLuint textureIndex, int textureID, con
 
     /// Put the texture index value in the Sampler uniform.
     GLuint uniformID = glGetUniformLocation(_programID, uniformName);
-    glUniform1i(uniformID, textureIndex);
+    glProgramUniform1i(_programID, uniformID, textureIndex);
 
     _textures[textureIndex].ID = textureID;
     _textures[textureIndex].target = target;
